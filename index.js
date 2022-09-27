263d8010c9fcfd148fa7695a99969064b5b3e83c
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-09-27 16:07:16 +0100

    complete add part

--- index.js
+++ index.js
@@ -12,40 +12,6 @@ function addBookToList(book) {
     `;
   container.append(div);
 }
-
-// get book from local storage
-function getBook() {
-  let book;
-  if (localStorage.getItem('books') === null) {
-    book = [];
-  } else {
-    book = JSON.parse(localStorage.getItem('books'));
-  }
-  return book;
-}
-
-// function to add book to local storage
-function addBook(book) {
-  const books = getBook();
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-// Delete book from local storage
-function deleteBook() {
-  const books = getBook();
-  books.forEach((book, index) => {
-    books.splice(index, 1);
-  });
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-// Clear form input's values
-function clearFields() {
-  document.querySelector('.title').value = '';
-  document.querySelector('.author').value = '';
-}
-
 // function to add book and display
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
@@ -54,21 +20,4 @@ form.addEventListener('submit', (e) => {
   const author = document.querySelector('.author').value;
   const obj = { title, author };
   addBookToList(obj);
-  addBook(obj);
-  clearFields();
-});
-
-// function to remove book
-function removeBook(el) {
-  if (el.classList.contains('remove-btn')) {
-    el.parentElement.remove();
-  }
-}
-
-// Remove book
-document.querySelector('.container').addEventListener('click', (e) => {
-  // delete elements from screen
-  removeBook(e.target);
-  // remove book from local storage
-  deleteBook(e.target);
 });
